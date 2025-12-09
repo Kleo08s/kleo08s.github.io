533f39d267dd3bd4d1cd405ed75e0b133488fce1
Kleo08s <kleandrodrizari@gmail.com>
2025-12-09 22:03:26 +0100

    navbar i guess?

---
+++ src/lib/components/app-sidebar.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+import HouseIcon from "@lucide/svelte/icons/house";
+import BookMarked from "@lucide/svelte/icons/book-marked";
+import Mail from "@lucide/svelte/icons/mail";
+import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+ 
+const items = [
+    {
+        title: "Home",
+        url: "#",
+        icon: HouseIcon,
+    },
+    {
+        title: "Projects",
+        url: "#",
+        icon: BookMarked,
+    },
+    {
+        title: "Contacts",
+        url: "#",
+        icon: Mail,
+    },
+];
+</script>
+ 
+<Sidebar.Root style="padding-top: 3rem; margin-left: 15rem">
+ <Sidebar.Content>
+  <Sidebar.Group>
+   <Sidebar.GroupLabel style="font-size: 1.75rem">Kleo08s</Sidebar.GroupLabel>
+   <Sidebar.GroupContent>
+    <Sidebar.Menu style="margin-top: 1rem">
+     {#each items as item (item.title)}
+      <Sidebar.MenuItem style="size: 4rem">
+       <Sidebar.MenuButton>
+        {#snippet child({ props })}
+         <a href={item.url} {...props}>
+          <item.icon />
+          <span>{item.title}</span>
+         </a>
+        {/snippet}
+       </Sidebar.MenuButton>
+      </Sidebar.MenuItem>
+     {/each}
+    </Sidebar.Menu>
+   </Sidebar.GroupContent>
+  </Sidebar.Group>
+ </Sidebar.Content>
+</Sidebar.Root>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,9 @@
 	import favicon from '$lib/assets/favicon.svg';
 	import { ModeWatcher } from "mode-watcher";
 
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import AppSidebar from "$lib/components/app-sidebar.svelte";
+
 	let { children } = $props();
 </script>
 
@@ -13,4 +16,11 @@
 </svelte:head>
 
 <ModeWatcher />
-{@render children()}
+
+<Sidebar.Provider style="margin-left: 18rem; width: auto">
+  <AppSidebar />
+  <main>
+    <Sidebar.Trigger />
+    {@render children?.()}
+  </main>
+</Sidebar.Provider>
