2d285ac9cfc11d62485d8d1f0eb3b8d6fa113e64
Kleo08s <kleandrodrizari@gmail.com>
2026-01-10 19:50:00 +0100

    removed useless things

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Sun, ExternalLink, Rss, Gamepad2, Music, Flame } from '@lucide/svelte';
+  import * as icon from '@lucide/svelte';
   
   import Navbar from "$lib/components/navbar.svelte";
 </script>
--- src/routes/contacts/+page.svelte
+++
@@ -1,8 +0,0 @@
-<div class="shadow-up"></div>
-
-<h1 style="position: fixed; z-index: 999; margin-left: 1rem; margin-top: -1rem">Contacts</h1>
-<div class="grid-main">
-  
-</div>
-
-<div class="shadow-bottom"></div>
