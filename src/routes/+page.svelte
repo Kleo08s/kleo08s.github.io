3c55b062e8d6085268480a498eb2e7b2ca65cef9
Kleo08s <kleandrodrizari@gmail.com>
2026-01-25 02:59:16 +0100

    redesigned profile card, added badge

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,47 +26,61 @@
     margin-bottom: 2.5rem;
   }
 
-  #banner-wrap {
-    height: 7.5rem;
-    border-radius: var(--radius-xl);
-    overflow: hidden;
-    position: relative;
-  }
-
-  #banner-wrap::after {
-    content: "";
-    position: absolute;
-    inset: 0;
-    box-shadow: inset 0 -8px 12px var(--card);
-    pointer-events: none;
-  }
-
   #banner {
     width: 100%;
-    height: 100%;
+    height: 7.5rem;
     object-fit: cover;
     filter: opacity(0.5);
+    border-radius: var(--radius-xl) var(--radius-xl) 0 0;
   }
 
   #profile #pfp {
-    width: 6.5rem;
-    height: 6.5rem;
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: -3rem;
+    width: 6rem;
+    height: 6rem;
+    margin-left: 2rem;
+    margin-top: -3.5rem;
     position: relative;
     border-radius: 100%;
     border: 1px solid var(--border);
   }
 
   #profile h2 {
-    text-align: center;
-    margin-top: 1rem;
+    margin-left: 1.5rem;
+    margin-top: 0.5rem;
   }
 
   #profile p {
     text-align: center;
     color: var(--muted-foreground);
+  }
+
+  #profile .name {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  #profile .name .badge {
+    background-color: var(--secondary);
+    padding: 0.25rem;
+    padding-left: 0.75rem;
+    padding-right: 0.75rem;
+    margin-top: 0.5rem;
+    display: flex;
+    gap: 0.5rem;
+    width: max-content;
+    height: 1.75rem;
+    align-items: center;
+    border-radius: var(--radius-lg);
+    border: 1px solid var(--border);
+  }
+
+  #profile .name .badge b {
+    font-size: 13px;
+  }
+
+  #profile .name .badge img {
+    height: 1rem;
   }
 </style>
 
@@ -75,11 +89,15 @@
 <div class="main">
   <div class="grid1">
     <div id="profile" class="element" style="padding-top: 0; padding-left: 0; padding-right: 0">
-      <div id="banner-wrap">
-        <img id="banner" src={config.profile.banner} alt="Banner">
-      </div>
+      <img id="banner" src={config.profile.banner} alt="Banner">
       <img id="pfp" src={config.profile.pfp} alt="PFP">
-      <h2>{config.profile.name}</h2>
+      <div class="name">
+        <h2>{config.profile.name}</h2>
+        <div class="badge">
+          <img src="https://cdn.discordapp.com/clan-badges/398627612299362304/9c28f5002410f1d1679d0e6a99609ef4.png?size=64" alt="GD Badge">
+          <b>GD</b>
+        </div>
+      </div>
       <p>{config.profile.bio}</p>
     </div>
     <div class="element">
